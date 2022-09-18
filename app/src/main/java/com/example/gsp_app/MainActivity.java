7ef93ade78e4d01e19031ec8c93c0090446b7a07
LuaySB <luai.github.6111998>
2022-09-18 17:57:00 +0200

    Added Bottom Navigation Bar.

--- app/src/main/java/com/example/gsp_app/MainActivity.java
+++ app/src/main/java/com/example/gsp_app/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.gsp_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.gsp_app.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+        switch (item.getItemId()){
+            case R.id.home:
+                replaceFragment(new HomeFragment());
+                break;
+            case R.id.search:
+                replaceFragment(new SearchFragment());
+                break;
+            case R.id.compare:
+                replaceFragment(new CompareFragment());
+                break;
+            case R.id.shopping_list:
+                replaceFragment(new ShoppingListFragment());
+                break;
+        }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
