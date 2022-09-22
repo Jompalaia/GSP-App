5bef12a004aba4d88acb58bbfe4973394f0743f7
gabri <gabriel.fagerslett@outlook.com>
2022-09-22 14:15:15 +0200

    Design edits and added activities

--- app/src/main/java/com/example/gsp_app/MainActivity.java
+++ app/src/main/java/com/example/gsp_app/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.gsp_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton menuBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        menuBtn = findViewById(R.id.menu_btn);
+
+        menuBtn.setOnClickListener((v)->showMenu());
+
     }
+
+    void showMenu(){
+        PopupMenu popupMenu = new PopupMenu(MainActivity.this,menuBtn);
+        popupMenu.getMenu().add("Logout");
+        popupMenu.show();
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+
+                if(menuItem.getTitle()=="Logout"){
+                    //FirebaseAuth.getInstance().signOut();
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                    finish();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
 }
+
